6e6afe2e235571659f45373d797d1e631044e0be
vicenta <vicenta_blanch@hotmail.es>
2016-01-26 16:35:28 +0100

    web instalada

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simonApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the simonApp
+ */
+angular.module('simonApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simonApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the simonApp
+ */
+angular.module('simonApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
